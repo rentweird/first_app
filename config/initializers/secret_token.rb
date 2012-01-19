eb97abfa6c430a79fee9e982dd0596146b868694
U-RentWeird-HP\Rent Weird <Rent Weird@RentWeird-HP.(none)>
2012-01-19 14:27:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6a9d30c179d99ea6ff2f2acaa954a66640f4d41881d85e50a2809081d8b823518d7d6d4b6f0d6996d732754bc5356e24f242e11fb01b16ce5947ef1dd1927bbd'
